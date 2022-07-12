e3a2abd083022d2bfbaa076d975e5a51e84f7f90
Priscilla Rojas <pri.reds174@gmail.com>
2022-07-12 15:43:17 -0400

    Updated About Me

--- src/components/About.js
+++ src/components/About.js
@@ -31,12 +31,14 @@ const About = () => {
                                 />
                             </h1>
                             </Fade>
-                            <p>I am a software engineer with a passion for learning and creating.I am a driven individual that values quality over quantity.</p>
+                            <p>I am a driven software engineer with a passion for learning and creating.</p>
                             <br></br>
-                            <p>My perseverance in my different job roles has allowed me
-                            to move up into management roles. I give my all and am driven to excel at whatever is thrown my way.</p>
+                            <p>creating. My experience in customer service can be
+translated into a passion for solving problems and providing solutions for others. I am meticulous and boldly face new
+intellectual challenges.</p>
                             <br></br>
-                            <p>Over the years, different parts of my career have taught me, “You don’t know if you don’t try.” I want to provide simple solutions to everyday problems people face. With my determination, empathy, and curiosity, I know I can assimilate to the needs of a business to better help others.</p>
+                            <p>I am always willing to learn new tools and skills to enhance my team. With my determination,
+empathy, and curiosity, I am constantly honing my strengths and learning my deficiencies.</p>
                         </div>
                         <div className="image-wrapper">
                          <img src={Selfie} className="rounded float-end" alt="..."/>
