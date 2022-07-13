4aad79588b5615ada2c1f932927f42e062dabbd8
prireds174 <pri.reds174@gmail.com>
2022-07-13 03:48:31 -0400

    troubleshooting

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,15 +3,7 @@ import './styles/card.scss'
 
 const Card = ({ heading, paragraph, imgUrl, projectLink }) => {
   return (
-    <div
-      className="card"
-      style={{
-        backgroundImage:
-          "linear-gradient(to bottom, rgba(245, 246, 252, 0), rgba(0, 0, 0, 0.2)),url(" +
-          imgUrl +
-          ")",
-      }}
-    >
+    <div className="card">
       <div className="content">
         <h1 className="header">{heading}</h1>
         <p className="text">{paragraph}</p>
